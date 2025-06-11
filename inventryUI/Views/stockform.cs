1d1f1c2de159864d8aa6b9c8c72dbc1ccf40de1e
Ameenn07 <ameen072005@gmail.com>
2025-06-11 21:15:52 +0530

    adding branch

---
+++ inventryUI/Views/stockform.cs
@@ -0,0 +1,40 @@
+namespace sirimath
+{
+    public partial class stockform : Form
+    {
+        public stockform()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stockform_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
